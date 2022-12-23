3fbe13bd55708ebcc9f59253355247230d2af716
이상우 <mungjin4966@gmail.com>
2022-12-23 11:48:53 +0900

    4375번 문제 해결

---
+++ src/Mathematics/OneNumber/Main.java
@@ -0,0 +1,25 @@
+package Mathematics.OneNumber;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNextInt()) {
+            int n = sc.nextInt();
+            int num = 0;
+            for(int i = 1; ; i++) {
+                num = (num * 10) + 1;
+                num = num % n;
+                if(num == 0) {
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
+
+
+    }
+}
+
