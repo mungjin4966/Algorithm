9de2630159b9ad46d9a20a45cdf2377823fba084
이상우 <mungjin4966@gmail.com>
2022-12-27 23:53:59 +0900

    Bj2309 백준 작업중

---
+++ src/bruteforce/bj2309/Bj2309.java
@@ -0,0 +1,38 @@
+package bruteforce.bj2309;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Bj2309 {
+
+    public StringBuffer dwarf() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+        int numbers[] = new int[9];
+        int sum = 0;
+        
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.parseInt(br.readLine());
+            sum += numbers[i];
+        }
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 9; j++) {
+                if(sum - numbers[i]-numbers[j] == 100){
+                    numbers[i] = 0;
+                    numbers[j] = 0;
+                    Arrays.sort(numbers);
+                    for (int k = 2; k < 9; k++) {
+                        System.out.println(numbers[k]);
+                    }
+                }
+                break;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ src/bruteforce/bj2309/Main.java
@@ -0,0 +1,12 @@
+package bruteforce.bj2309;
+
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Bj2309 bj2309 = new Bj2309();
+        bj2309.dwarf();
+    }
+}
