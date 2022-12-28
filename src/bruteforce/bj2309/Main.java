216f8a33f6135f037ae24a20c8cad9c5fec7210f
이상우 <mungjin4966@gmail.com>
2022-12-28 15:44:09 +0900

    2309번 백준 문제 해결

--- src/bruteforce/bj2309/Main.java
+++ src/bruteforce/bj2309/Main.java
@@ -10,26 +10,25 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
         int numbers[] = new int[9];
         int sum = 0;
 
-        for (int i = 0; i < numbers.length; i++) {
+        for (int i = 0; i < 9; i++) {
             numbers[i] = Integer.parseInt(br.readLine());
             sum += numbers[i];
         }
 
         for (int i = 0; i < 8; i++) {
             for (int j = i+1; j < 9; j++) {
-                if(sum - numbers[i]-numbers[j] == 100){
-                    numbers[i] = 0;
-                    numbers[j] = 0;
+                if(sum - numbers[i] - numbers[j] == 100){
+                    numbers[i] = 100;
+                    numbers[j] = 100;
                     Arrays.sort(numbers);
-                    for (int k = 2; k < 9; k++) {
+                    for (int k = 0; k <= 6; k++) {
                         System.out.println(numbers[k]);
                     }
+                    return;
                 }
-                break;
             }
         }
     }
--- test/bruteforce/bj2309/Bj2309Test.java
+++ test/bruteforce/bj2309/Bj2309Test.java
@@ -1,15 +1,16 @@
 package bruteforce.bj2309;
 
+import junit.framework.TestCase;
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
 import java.io.*;
 
-class Bj2309Test {
+public class Bj2309Test extends TestCase {
 
     @Test
-    void dwarf() throws IOException {
+    public void testDwarf() throws IOException {
         Bj2309 bj2309 = new Bj2309();
-
         String input = "20\n7\n23\n19\n10\n15\n25\n8\n13";
         OutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
@@ -19,9 +20,11 @@ class Bj2309Test {
 
         StringBuffer value = bj2309.dwarf();
         StringBuffer target = new StringBuffer();
-        target.append("7\n8\n10\n13\n19\n20\n23");
+        target.append("7\n").append("8\n").append("10\n").append("13\n").append("19\n").append("20\n").append("23\n");
 
-        Assert.assertEquals(value.toString(), target.toString());
+        String result = value.toString();
+        String comparision = target.toString();
 
+        Assert.assertEquals(result, comparision);
     }
 }
