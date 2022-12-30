8ff803f4a86ec2891b8a98a347b14d33c8955dee
이상우 <mungjin4966@gmail.com>
2022-12-30 23:31:29 +0900

    1107번 문제 해결중

--- src/bruteforce/bj1107/Bj1107.java
+++ src/bruteforce/bj1107/Bj1107.java
@@ -1,4 +1,57 @@
 package bruteforce.bj1107;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Bj1107 {
+
+    public int live = 100;
+
+    public int remote() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String n = br.readLine();
+        char[] target = n.toCharArray();
+
+        int broken = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int brokenNums[] = new int[broken];
+        for (int i = 0; i < broken; i++) {
+            brokenNums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        boolean remocon[] = new boolean[10];
+        Arrays.fill(remocon, true);
+
+        for (int i = 0; i < remocon.length; i++) {
+            for (int j = 0; j < brokenNums.length ; j++) {
+                if(i == brokenNums[j]){
+                    remocon[i] = false;
+                }
+            }
+        }
+
+        int cnt = 0;
+        int[] value = new int[n.length()];
+
+        if (live == Integer.parseInt(n)) {
+            System.out.println(cnt);
+        }
+
+        for (int i = 0; i < target.length; i++) {
+            for (int l = 0; l < remocon.length; l++) {
+                if(Integer.parseInt(String.valueOf(target[i])) == l && remocon[l]){
+                    value[i] = l;
+                }
+                
+            }
+        }
+
+
+        return 0;
+    }
 }
--- src/bruteforce/bj1107/Main.java
+++ src/bruteforce/bj1107/Main.java
@@ -1,4 +1,10 @@
 package bruteforce.bj1107;
 
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] agrs) throws IOException {
+        Bj1107 bj1107 = new Bj1107();
+        bj1107.remote();
+    }
 }
