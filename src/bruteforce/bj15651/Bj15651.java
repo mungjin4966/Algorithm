9e9ffa1c2105fd0104ece2e09d5e0d5231fb36d2
이상우 <mungjin4966@gmail.com>
2023-01-06 20:53:35 +0900

    bj15651번 문제 해결

--- src/bruteforce/bj15651/Bj15651.java
+++ src/bruteforce/bj15651/Bj15651.java
@@ -1,4 +1,42 @@
 package bruteforce.bj15651;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Bj15651 {
+
+    public static StringBuffer sb;
+    public static int N, M;
+    public static int[] numbers;
+
+    public StringBuffer numberList() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        numbers = new int[M];
+
+        sb = new StringBuffer();
+        sequence(0);
+
+        return sb;
+    }
+
+    public void sequence(int depth) {
+        if (depth == M) {
+            for (int val : numbers) {
+                sb.append(val + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            numbers[depth] = i + 1;
+            sequence(depth + 1);
+        }
+    }
 }
+
